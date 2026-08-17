3e308902d95dce4750123eab3b0710bbef15087f
lungmat8 <lungmat8@users.noreply.github.com>
2026-08-17 13:35:17 +0000

    deploy: 15b940deaec363237829b0addcf4f2a02d0fab72

--- api/trait.impl/hal_adc/trait.ViAdc.js
+++ api/trait.impl/hal_adc/trait.ViAdc.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["driver_adc_sim",[["impl ViAdc for <a class=\"struct\" href=\"driver_adc_sim/struct.SimAdc.html\" title=\"struct driver_adc_sim::SimAdc\">SimAdc</a>",0]]]]);
+    const implementors = Object.fromEntries([["driver_adc_sim",[["impl <a class=\"trait\" href=\"hal_adc/trait.ViAdc.html\" title=\"trait hal_adc::ViAdc\">ViAdc</a> for <a class=\"struct\" href=\"driver_adc_sim/struct.SimAdc.html\" title=\"struct driver_adc_sim::SimAdc\">SimAdc</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[155]}
+//{"start":59,"fragment_lengths":[243]}
--- api/trait.impl/hal_i2c/trait.ViI2c.js
+++ api/trait.impl/hal_i2c/trait.ViI2c.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["driver_i2c_gpio",[["impl&lt;G: ViGpio&gt; ViI2c for <a class=\"struct\" href=\"driver_i2c_gpio/struct.BitBangI2c.html\" title=\"struct driver_i2c_gpio::BitBangI2c\">BitBangI2c</a>&lt;G&gt;",0]]]]);
+    const implementors = Object.fromEntries([["driver_i2c_gpio",[["impl&lt;G: <a class=\"trait\" href=\"hal_gpio/trait.ViGpio.html\" title=\"trait hal_gpio::ViGpio\">ViGpio</a>&gt; <a class=\"trait\" href=\"hal_i2c/trait.ViI2c.html\" title=\"trait hal_i2c::ViI2c\">ViI2c</a> for <a class=\"struct\" href=\"driver_i2c_gpio/struct.BitBangI2c.html\" title=\"struct driver_i2c_gpio::BitBangI2c\">BitBangI2c</a>&lt;G&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[196]}
+//{"start":59,"fragment_lengths":[376]}
