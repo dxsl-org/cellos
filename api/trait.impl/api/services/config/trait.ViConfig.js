55095beb7ba53a4acde85d1ff153c05b9cf7bb0f
lungmat8 <lungmat8@users.noreply.github.com>
2026-09-13 23:09:16 +0000

    deploy: adfd1bd0d6da4f892da6e9c38b9f32ce5863616e

--- api/trait.impl/api/services/config/trait.ViConfig.js
+++ api/trait.impl/api/services/config/trait.ViConfig.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["app_shell",[["impl ViConfig for <a class=\"struct\" href=\"app_shell/config_client/struct.ConfigClient.html\" title=\"struct app_shell::config_client::ConfigClient\">ConfigClient</a>",0]]]]);
+    const implementors = Object.fromEntries([["app_shell",[["impl <a class=\"trait\" href=\"api/services/config/trait.ViConfig.html\" title=\"trait api::services::config::ViConfig\">ViConfig</a> for <a class=\"struct\" href=\"app_shell/config_client/struct.ConfigClient.html\" title=\"struct app_shell::config_client::ConfigClient\">ConfigClient</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[190]}
+//{"start":59,"fragment_lengths":[310]}
--- api/trait.impl/hal_pwm/trait.ViPwm.js
+++ api/trait.impl/hal_pwm/trait.ViPwm.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["driver_pwm_gpio",[["impl&lt;G: ViGpio&gt; ViPwm for <a class=\"struct\" href=\"driver_pwm_gpio/struct.BitBangPwm.html\" title=\"struct driver_pwm_gpio::BitBangPwm\">BitBangPwm</a>&lt;G&gt;",0]]]]);
+    const implementors = Object.fromEntries([["driver_pwm_gpio",[["impl&lt;G: <a class=\"trait\" href=\"hal_gpio/trait.ViGpio.html\" title=\"trait hal_gpio::ViGpio\">ViGpio</a>&gt; <a class=\"trait\" href=\"hal_pwm/trait.ViPwm.html\" title=\"trait hal_pwm::ViPwm\">ViPwm</a> for <a class=\"struct\" href=\"driver_pwm_gpio/struct.BitBangPwm.html\" title=\"struct driver_pwm_gpio::BitBangPwm\">BitBangPwm</a>&lt;G&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[196]}
+//{"start":59,"fragment_lengths":[376]}
