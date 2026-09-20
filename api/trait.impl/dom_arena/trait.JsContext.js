548e164a792361d961221fe5621a1590b10ccf08
lungmat8 <lungmat8@users.noreply.github.com>
2026-09-20 13:57:53 +0000

    deploy: 939def09997f478416fad1071ef72590f642b961

--- api/trait.impl/dom_arena/trait.JsContext.js
+++ api/trait.impl/dom_arena/trait.JsContext.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["ocel",[["impl <a class=\"trait\" href=\"ocel/js/trait.JsContext.html\" title=\"trait ocel::js::JsContext\">JsContext</a> for <a class=\"struct\" href=\"ocel/js/bridge/struct.Tier2JsBridge.html\" title=\"struct ocel::js::bridge::Tier2JsBridge\">Tier2JsBridge</a>",0]]],["ocel_js",[["impl JsContext for <a class=\"struct\" href=\"ocel_js/engine/struct.OcelJsExecutionContext.html\" title=\"struct ocel_js::engine::OcelJsExecutionContext\">OcelJsExecutionContext</a>",0]]]]);
+    const implementors = Object.fromEntries([["ocel",[["impl <a class=\"trait\" href=\"ocel/js/trait.JsContext.html\" title=\"trait ocel::js::JsContext\">JsContext</a> for <a class=\"struct\" href=\"ocel/js/bridge/struct.Tier2JsBridge.html\" title=\"struct ocel::js::bridge::Tier2JsBridge\">Tier2JsBridge</a>",0]]],["ocel_js",[["impl <a class=\"trait\" href=\"dom_arena/trait.JsContext.html\" title=\"trait dom_arena::JsContext\">JsContext</a> for <a class=\"struct\" href=\"ocel_js/engine/struct.OcelJsExecutionContext.html\" title=\"struct ocel_js::engine::OcelJsExecutionContext\">OcelJsExecutionContext</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[269,202]}
+//{"start":59,"fragment_lengths":[269,302]}
--- api/trait.impl/dom_arena/trait.JsEngine.js
+++ api/trait.impl/dom_arena/trait.JsEngine.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["ocel",[["impl JsEngine for <a class=\"struct\" href=\"ocel/js/bridge/struct.Tier2JsBridge.html\" title=\"struct ocel::js::bridge::Tier2JsBridge\">Tier2JsBridge</a>",0]]],["ocel_js",[["impl JsEngine for <a class=\"struct\" href=\"ocel_js/engine/struct.OcelJsServiceEngine.html\" title=\"struct ocel_js::engine::OcelJsServiceEngine\">OcelJsServiceEngine</a>",0]]]]);
+    const implementors = Object.fromEntries([["ocel",[["impl <a class=\"trait\" href=\"dom_arena/trait.JsEngine.html\" title=\"trait dom_arena::JsEngine\">JsEngine</a> for <a class=\"struct\" href=\"ocel/js/bridge/struct.Tier2JsBridge.html\" title=\"struct ocel::js::bridge::Tier2JsBridge\">Tier2JsBridge</a>",0]]],["ocel_js",[["impl <a class=\"trait\" href=\"dom_arena/trait.JsEngine.html\" title=\"trait dom_arena::JsEngine\">JsEngine</a> for <a class=\"struct\" href=\"ocel_js/engine/struct.OcelJsServiceEngine.html\" title=\"struct ocel_js::engine::OcelJsServiceEngine\">OcelJsServiceEngine</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[171,192]}
+//{"start":59,"fragment_lengths":[269,290]}
